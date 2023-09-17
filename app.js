5b0190642b1fac5f85a80d93f23504a6de88d316
Denis Deaf <98868526+telesochi@users.noreply.github.com>
2023-09-17 08:59:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (navigator.serviceWorker.controller) {
 console.log('[PWA Builder] active service worker found, no need to register')
 } else {
-navigator.serviceWorker.register('/sw.js', {
+navigator.serviceWorker.register('sw.js', {
 scope: 'https://telesochi.github.io/radiored/index.html'
 }).then(function(reg) {
 console.log('Service worker has been registered for scope:'+ reg.scope);
