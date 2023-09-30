afdb0bb7e77152b8b0f23c83a734b85c646cecc1
Denis Deaf <98868526+telesochi@users.noreply.github.com>
2023-09-30 08:53:28 +0300

    Add files via upload

---
+++ scripts/songs.js
@@ -0,0 +1,70 @@
+let songs = [
+  {
+    name: "Вести ФМ",
+    cover:
+      "https://fmplay.ru/img/vesti.webp",
+    artist: "новости / разговорное",
+    audio: "https://icecast-vgtrk.cdnvideo.ru/vestifm_aac_32kbps",
+    id: 0,
+  },
+  {
+    name: "Маяк",
+    cover:
+      "https://fmplay.ru/img/mayak.webp",
+    artist: "новости / разговорное",
+    audio: "https://icecast-vgtrk.cdnvideo.ru/mayakfm_mp3_64kbps",
+    id: 1,
+  },
+  {
+    name: "Комсомольская правда",
+    cover:
+      "https://fmplay.ru/img/kpravda.webp",
+    artist: "новости / разговорное",
+    audio: "https://kpradio.hostingradio.ru:8000/64",
+    id: 2,
+  },
+  {
+    name: "Соловьёв ФМ",
+    cover:
+      "https://telesochi.github.io/radiored/logo/solovievfm.jpg",
+    artist: "новости / разговорное",
+    audio: "https://solovievfm.hostingradio.ru/solovievfm32.aacp",
+    id: 3,
+  },
+  {
+    name: "Звезда",
+    cover:
+      "https://fmplay.ru/img/zvezda.webp",
+    artist: "новости / разговорное",
+    audio: "https://fed.fmplay.ru:8000/zvezda-32.aac",
+    id: 4,
+  },
+  {
+    name: "Радио Культура",
+    cover:
+      "https://fmplay.ru/img/kultura.webp",
+    artist: "Разговоры о музыке, кино, искусстве, истории, образовании...",
+    audio: "https://fed.fmplay.ru:8000/kultura-32.aac",
+    id: 5,
+  },
+
+
+
+
+
+
+{
+    name: "РетроХИТ",
+    cover:
+      "https://radiopotok.mobi/f/station/512/1570.png",
+    artist: "музыкальное радио",
+    audio: "https://retro.amgradio.ru/Retro",
+    id: 6,
+  },
+
+
+
+
+
+
+];
